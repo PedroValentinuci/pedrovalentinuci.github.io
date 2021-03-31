0b20e011ee5eaaeb270befe6333b9edc733d602b
Pedro Valentinuci <30358845+PedroValentinuci@users.noreply.github.com>
2021-03-31 15:38:38 -0300

    aula 8 completa
    a moldura ta meio bugada precisa dar uma olhada naquela bosta

--- sub_pages/imersaoDev/aula8/aula8.js
+++ sub_pages/imersaoDev/aula8/aula8.js
@@ -1,5 +1,6 @@
 var cartaPedro = {
     nome: "Seiya de pegaso",
+    imagem: "https://i.pinimg.com/originals/e9/c1/77/e9c1778727e6c604ace92a2ec73ce52b.jpg",
     atributos: {
         ataque: 80,
         defesa: 60,
@@ -9,6 +10,7 @@ var cartaPedro = {
 
 var cartaSophia = {
     nome: "Bulbasaur",
+    imagem: "https://i.pinimg.com/originals/3b/78/47/3b7847675982776e5219e12a680ecd84.png",
     atributos: {
         ataque: 70,
         defesa: 65,
@@ -18,6 +20,7 @@ var cartaSophia = {
 
 var cartaPlayer3 = {
     nome: "Lorde Darth Vader",
+    imagem: "https://images-na.ssl-images-amazon.com/images/I/41i-0NH0q9L._SX328_BO1,204,203,200_.jpg",
     atributos: {
         ataque: 88,
         defesa: 62,
@@ -39,20 +42,28 @@ function sortearCarta() {
         numeroCartaJogador = parseInt(Math.random() * 3)
     }
     cartaJogador = cartas[numeroCartaJogador]
-    console.log(cartaJogador)
 
     document.getElementById("btnSortear").disabled = true
     document.getElementById("btnJogar").disabled = false
-    exibirOpcoes()
+
+    exibeCartaJogador()
 }
 
-function exibirOpcoes() {
-    var opcoes = document.getElementById("opcoes")
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    
+    divCartaJogador.style.backgroundImage=`url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
     var opcoesTexto = ""
+
     for (var atributo in cartaJogador.atributos) {
-        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo +  " " + cartaJogador.atributos[atributo] + "<br>"
     }
-    opcoes.innerHTML = opcoesTexto
+
+    var html = "<div id= 'opcoes class='carta-status'>"
+
+    divCartaJogador.innerHTML = nome + html + "</div>" + opcoesTexto 
 }
 
 function obtemAtributoSelecionado() {
@@ -65,14 +76,33 @@ function obtemAtributoSelecionado() {
 }
 
 function jogar() {
+    var divResultado = document.getElementById("resultado")
     var atributoSelecionado = obtemAtributoSelecionado()
     
     if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
-        alert("Você venceu")
+        htmlResultado = "<p class='resultado-final'>Parabéns você venceu!!</p>"
     } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
-        alert("Você perdeu meu consagra")
+        htmlResultado = "<p class='resultado-final'>Perdeu meu mano!!</p>"
     } else {
-        alert("Empatou")
+        htmlResultado = "<p class='resultado-final'>Vocês empataram!!</p>"
     }
-    console.log(cartaMaquina)
+    divResultado.innerHTML = htmlResultado
+    exibeCartaMaquina()
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    
+    divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo +  " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id= 'opcoes class='carta-status'>"
+
+    divCartaMaquina.innerHTML = nome + html + "</div>" + opcoesTexto 
 }
